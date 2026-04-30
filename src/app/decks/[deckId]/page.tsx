5d63added49f76d43d9ed4d30e54ba6e35a635af
tanahiro2010 <herentongkegu087@gmail.com>
2026-04-30 17:23:38 +0900

    feat: デッキメタデータ生成機能を追加

--- src/app/decks/[deckId]/page.tsx
+++ src/app/decks/[deckId]/page.tsx
@@ -1,12 +1,41 @@
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 import { QuizClient } from "@/components/quiz-client";
+import { title } from "process";
+
 
 export const dynamic = "force-dynamic";
 
 type DeckPageProps = {
   params: Promise<{ deckId: string }>;
 };
+
+export async function generateMetadata({ params }: DeckPageProps): Promise<Metadata> {
+  const { deckId } = await params;
+  const deck = await prisma.deck.findFirst({
+    where: { id: deckId, isPublished: true },
+    include: {
+      questions: {
+        orderBy: { orderIndex: "asc" },
+        include: {
+          choices: {
+            orderBy: { orderIndex: "asc" },
+          },
+        },
+      },
+    },
+  });
+  if (!deck) return {
+    title: "404 問題が見つかりません",
+    description: "問題がみつかりません"
+  }
+
+  return {
+    title: deck.title,
+    description: `なんでも問題集に投稿された問題集「${title}」を今すぐプレイしましょう！`
+  }
+} 
 
 export default async function DeckPage({ params }: DeckPageProps) {
   const { deckId } = await params;
